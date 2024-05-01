889c4f472f62c49dfa5995606ca6eb9fb01d1b25
Santicm23 <Santicastrom@outlook.com>
2024-05-01 14:29:39 -0500

    adding clients endpoint

--- src/routers/sales-routers.ts
+++ src/routers/sales-routers.ts
@@ -4,6 +4,7 @@ import {
   createBill,
   getAllBills,
   getAllBillsByClient,
+  getAllClients,
   getBillStatsByPeriod,
   sendBillingEmail,
 } from '../controllers'
@@ -17,6 +18,8 @@ import { CreateBillDto, SendBillingEmailDto } from '../dtos'
 const router = express.Router()
 
 router.post('/', [validateSalesRole, validateDto(CreateBillDto)], createBill)
+
+router.get('/clients/all', [validateSalesOrAdminRole], getAllClients)
 
 router.get('/all', [validateSalesOrAdminRole], getAllBills)
 
